bad77d914f3d36fe463a7fa28ac6598cb9721ed6
and-re10 <andreandre_1234@hotmail.com>
2020-06-18 23:45:58 +0200

    finitions pour avoir une table plus propre

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,22 +17,42 @@
                     You are logged in!
                 </div>
 
-                <div class="container">
-                    @foreach ($users as $user)
-                        <div class="row justify-content-between">
-                            <p>{{$user->name}}</p>
-                            {{-- J'utilise la gate pour verifier si l'utilisateur connecté est le meme que l'utilisateur affiché et comme parametre j'utilise la variable $user qui est dans le foreach --}}
-                            {{-- La meme chose pour la policy update et delete --}}
-                            
-                            @can('update', $user)
-                                <div class="row">
-                                <button type="button" class="btn btn-warning">Edit</button>
-                                <button type="button" class="btn btn-danger">Delete</button>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($users as $user)
+                                        <tr>
+                                            <td>{{$user->name}}</td>
+                                            <td>
+                                                {{-- J'utilise la gate pour verifier si l'utilisateur connecté est le meme que l'utilisateur affiché et comme parametre j'utilise la variable $user qui est dans le foreach --}}
+                                                {{-- La meme chose pour la policy update et delete --}}
+                                                @can('update', $user)
+                                                    <form class="d-flex justify-content-end" action="" method="POST" enctype="multipart/form-data">
+                                                        <a href="" class="btn btn-warning mr-2">Éditer</a>
+                                                        <button type="submit" class="btn btn-danger">Supprimer</button>
+                                                    </form>
+                                                @endcan
+                                                
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
                             </div>
-                            @endcan
-                            
+                            <!-- /.card-body -->
                         </div>
-                    @endforeach
+                        <!-- /.card -->
+                    </div>
                 </div>
             </div>
         </div>
